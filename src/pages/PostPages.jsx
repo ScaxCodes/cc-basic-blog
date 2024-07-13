0b030bc0bb42afa5c5994eb3b966e1ef04d27e80
ScaxCodes <116537717+ScaxCodes@users.noreply.github.com>
2024-07-13 06:56:07 +0200

    Add dynamic single post routing and component

--- src/pages/PostPages.jsx
+++ src/pages/PostPages.jsx
@@ -1,40 +1,33 @@
 import { useFetch } from "../useFetch";
 import { URLS } from "../constants";
+import { useParams } from "react-router-dom";
 
 export function PostPages() {
-  const { data: postData, isLoading, isError } = useFetch(URLS.POSTS + "/1");
+  const { memberId } = useParams();
+
+  // TODO: Conditional range should be fetch dynamically by API
+  if (memberId < 1 || memberId > 200) {
+    return <h1>Error 404</h1>;
+  }
+
+  const {
+    data: postData,
+    isLoading,
+    isError,
+  } = useFetch(URLS.POSTS + "/" + memberId);
 
   if (isLoading) return "Loading";
   if (isError) return "Error";
 
   return (
     <>
-      <div class="container">
-        <h1 class="page-title">
-          sunt aut facere repellat provident occaecati excepturi optio
-          reprehenderit
-        </h1>
-        <span class="page-subtitle">
-          By: <a href="user.html">Leanne Graham</a>
+      <div className="container">
+        <h1 className="page-title">{postData.title}</h1>
+        <span className="page-subtitle">
+          By: <a href="user.html">UserID: {postData.userId}</a>
         </span>
-        <div>
-          quia et suscipit suscipit recusandae consequuntur expedita et cum
-          reprehenderit molestiae ut ut quas totam nostrum rerum est autem sunt
-          rem eveniet architecto
-        </div>
+        <div>{postData.body}</div>
       </div>
     </>
   );
-}
-{
-  /* return (
-    <div className="container">
-      <h1 className="page-title">Posts</h1>
-      <div className="card-grid">
-        {postsData.map((post) => (
-          <Post key={post.id} post={post} />
-        ))}
-      </div>
-    </div>
-  ); */
 }
--- src/router.jsx
+++ src/router.jsx
@@ -3,6 +3,7 @@ import { Users } from "./pages/Users";
 import { Todos } from "./pages/Todos";
 import { Navbar } from "./Navbar";
 import { Posts } from "./pages/Posts";
+import { PostPages } from "./pages/PostPages";
 
 export const router = createBrowserRouter([
   {
@@ -13,7 +14,7 @@ export const router = createBrowserRouter([
         path: "/",
         children: [
           { index: true, element: <Posts /> },
-          { path: ":memberId", element: <h1>cool</h1> },
+          { path: ":memberId", element: <PostPages /> },
         ],
       },
       { path: "/users", element: <Users /> },
