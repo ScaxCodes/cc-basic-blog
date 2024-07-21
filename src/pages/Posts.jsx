8d9be644886e311ef99d9f8b5ea30afd174e088e
ScaxCodes <116537717+ScaxCodes@users.noreply.github.com>
2024-07-21 14:13:41 +0200

    Load posts data via router loader

--- src/pages/Posts.jsx
+++ src/pages/Posts.jsx
@@ -1,12 +1,10 @@
 import { Post } from "../Post";
-import { useFetch } from "../useFetch";
+import { useLoaderData, useNavigation } from "react-router-dom";
 import { URLS } from "../constants";
 
 export function Posts() {
-  const { data: postsData, isLoading, isError } = useFetch(URLS.POSTS);
+  const postsData = useLoaderData();
 
-  if (isLoading) return <div className="loading-spinner"></div>;
-  if (isError) return "Error";
   return (
     <div className="container">
       <h1 className="page-title">Posts</h1>
@@ -18,19 +16,3 @@ export function Posts() {
     </div>
   );
 }
-
-// 1. Map over object and render all posts, pass data as props to post comp
-// done
-// 2. Do the same for users and todos
-// done
-// 3. Think about the structure of your code, is it good?
-// done, refactored Home.jsx
-// 4. useOutletContext a better fit here?
-// done, don't think so, fetching with every new routing is fine, passing data to child, not to grand child
-// 5. Create a Post page that renders out the post title, and body.
-// done
-// 6. Create a User page that renders out the user name, company name, email, website, and address.
-// done
-
-// 7. Dynamic API requests for steps No. 5 and 6
-// skip, need to make another fetch request OR handle the isError status when request endpoint is 404
--- src/router.jsx
+++ src/router.jsx
@@ -3,6 +3,7 @@ import {
   Outlet,
   ScrollRestoration,
   createBrowserRouter,
+  useNavigation,
 } from "react-router-dom";
 import { Users } from "./pages/Users";
 import { Todos } from "./pages/Todos";
@@ -10,6 +11,7 @@ import { Navbar } from "./Navbar";
 import { Posts } from "./pages/Posts";
 import { PostPages } from "./pages/PostPages";
 import { UserPages } from "./pages/UserPages";
+import { URLS } from "./constants";
 
 export const router = createBrowserRouter([
   {
@@ -19,8 +21,16 @@ export const router = createBrowserRouter([
       { index: true, element: <Navigate to="/posts" /> },
       {
         path: "posts",
+
         children: [
-          { index: true, element: <Posts /> },
+          {
+            index: true,
+            element: <Posts />,
+            loader: ({ request: { signal } }) => {
+              console.log("executed loader...");
+              return fetch(URLS.POSTS, { signal });
+            },
+          },
           { path: ":postId", element: <PostPages /> },
         ],
       },
@@ -34,16 +44,28 @@ export const router = createBrowserRouter([
       { path: "todos", element: <Todos /> },
       { path: "*", element: <h1>404 - Page Not Found</h1> },
     ],
+    errorElement: (
+      <>
+        <Navbar />
+        <h1>Error occurred!</h1>
+      </>
+    ),
   },
 ]);
 
 function NavLayout() {
+  const { state } = useNavigation();
+  const isLoading = state === "loading";
+
   return (
     <>
       <Navbar />
-      <Outlet />
       {/* Just in case scroll bar is behaving strange */}
       <ScrollRestoration />
+      {isLoading && <div className="loading-spinner" />}
+      <div className={`container ${isLoading ? "loading" : ""}`}>
+        <Outlet />
+      </div>
     </>
   );
 }
